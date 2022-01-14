36a3a985ddd7a7349e39c37b16bb0256fd5c44cd
Brian Ronald <brian@ppcis.org>
2022-01-14 21:41:34 +0000

    Minor version change

--- Printer Toner/Program.cs
+++ Printer Toner/Program.cs
@@ -25,7 +25,7 @@ namespace IngameScript
         List<IMyTerminalBlock> Containers = new List<IMyTerminalBlock>();
         List<IMyAssembler> AllAssemblers = new List<IMyAssembler>();
         List<IMyAssembler> PrintAssemblers = new List<IMyAssembler>();
-        string Version = "Version 1.2.1";
+        string Version = "Version 1.2.2";
         MyIni ini = new MyIni();
         string ComponentSection = "Components";
         string PrinterSection = "Printer";
